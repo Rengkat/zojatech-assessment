b32ba3b6d50a225a104018e0fdbb5af70f18975a
Rengkat <successinmotion11@gmail.com>
2026-05-19 22:43:48 +0100

    add: redux store and appSlice

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import appSlice from "./features/appSlice";
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    appSlice,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
 });
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
