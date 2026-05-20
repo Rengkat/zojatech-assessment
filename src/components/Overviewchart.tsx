2fdb4fdb3532c9b64545eb01d6a14181acdc0595
Rengkat <successinmotion11@gmail.com>
2026-05-20 19:50:35 +0100

    add: mock data for charts

---
+++ src/components/Overviewchart.tsx
@@ -0,0 +1,16 @@
+const data = [
+  { month: "JAN", value: 620 },
+  { month: "FEB", value: 580 },
+  { month: "MAR", value: 410 },
+  { month: "APR", value: 390 },
+  { month: "MAY", value: 480 },
+  { month: "JUN", value: 860 },
+  { month: "JUL", value: 530 },
+  { month: "AUG", value: 490 },
+  { month: "SEP", value: 370 },
+  { month: "OCT", value: 420 },
+  { month: "NOV", value: 310 },
+  { month: "DEC", value: 390 },
+];
+
+const filters = ["Robbin Hood", "Amreitrade", "Fidelity", "Charles"];
