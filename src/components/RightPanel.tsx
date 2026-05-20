1621768b4a0517e9ed06975ff884a5345427266c
Rengkat <successinmotion11@gmail.com>
2026-05-20 20:28:14 +0100

    add: revenu component in right panel

--- src/components/RightPanel.tsx
+++ src/components/RightPanel.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Watchlist } from "./Watchlist";
+import { Revenue } from "./Revenue";
 
 export const RightPanel: React.FC = () => (
   <div className="flex flex-col gap-4 w-full xl:w-[280px] shrink-0">
     <Watchlist />
+    <Revenue />
   </div>
 );
